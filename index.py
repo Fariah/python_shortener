ff56a0bf9afa05997c506c6f442c97f8de0480f5
Fariah <darkclarus@gmail.com>
2020-08-09 13:11:58 +0300

    added 404 page

--- index.py
+++ index.py
@@ -1,12 +1,17 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, make_response, render_template
+app = Flask(__name__, template_folder="templates")
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World!</br>It would be a new telegram bot.'
+
+@app.errorhandler(404)
+def not_found(self):
+    """Page not found."""
+    return make_response(render_template("404.html"), 404)
 
 if __name__ == '__main__':
     app.run(debug=True)
